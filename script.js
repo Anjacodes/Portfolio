674361a9fb84e0c7d32ee0cb4026aa367333dbc4
Anjacodes <anja_schmidt7@posteo.de>
2021-11-08 17:30:08 -0600

    Add Linters

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-const hamMenu = document.getElementsByClassName("menu");
-const mobileMenu = document.getElementsByClassName("mobMenu");
-const closeMobMenu = document.getElementById("closeMenu");
+const hamMenu = document.getElementsByClassName('menu');
+const mobileMenu = document.getElementsByClassName('mobMenu');
+const closeMobMenu = document.getElementById('closeMenu');
 const mobLinks = document.querySelectorAll(".mobMenu > ul > li > a");
 
-const projectSection = document.getElementById("projects"); 
-const aboutmeSection = document.getElementById("aboutMe"); 
-const contactSection = document.getElementById("contact"); 
-
-hamMenu[0].addEventListener("click", function () {
-  mobileMenu[0].style.display = "block";
+hamMenu[0].addEventListener('click', function () {
+  mobileMenu[0].style.display = 'block';
 });
 
-closeMobMenu.addEventListener("click", function () {
-  mobileMenu[0].style.display = "none";
+closeMobMenu.addEventListener('click', function () {
+  mobileMenu[0].style.display = 'none';
 });
 
-for (i of mobLinks){
-  i.addEventListener('click',function (){
-    mobileMenu[0].style.display = "none";
+for (let i of mobLinks) {
+  i.addEventListener('click', function () {
+    mobileMenu[0].style.display = 'none';
   });
-}
-
+};
