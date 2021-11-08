da053dfd6cd31044b8bf7876d526d28b8aa407b5
Luis Abarca <angelgrd12@gmail.com>
2021-11-08 16:25:40 -0500

    Add functionality to each of the links on mobile menu.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const hamMenu = document.getElementsByClassName("menu");
 const mobileMenu = document.getElementsByClassName("mobMenu");
 const closeMobMenu = document.getElementById("closeMenu");
-const mobLinks = document.querySelectorAll(".mobMenu > ul > li");
+const mobLinks = document.querySelectorAll(".mobMenu > ul > li > a");
+
+const projectSection = document.getElementById("projects"); 
+const aboutmeSection = document.getElementById("aboutMe"); 
+const contactSection = document.getElementById("contact"); 
 
 hamMenu[0].addEventListener("click", function () {
   mobileMenu[0].style.display = "block";
@@ -10,3 +14,10 @@ hamMenu[0].addEventListener("click", function () {
 closeMobMenu.addEventListener("click", function () {
   mobileMenu[0].style.display = "none";
 });
+
+for (i of mobLinks){
+  i.addEventListener('click',function (){
+    mobileMenu[0].style.display = "none";
+  });
+}
+
