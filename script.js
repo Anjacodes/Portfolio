7d28da3589622e4d743648a75a5a4ba640179c52
Anjacodes <anja_schmidt7@posteo.de>
2021-11-08 17:57:30 -0600

    Fix Linter Errors

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const mobileMenu = document.getElementsByClassName('mobMenu');
 const closeMobMenu = document.getElementById('closeMenu');
 const mobLinks = document.querySelectorAll('.mobMenu > ul > li > a');
 
-hamMenu[0].addEventListener('click', function () {
+hamMenu[0].addEventListener('click', function selectHamMenu() {
   mobileMenu[0].style.display = 'block';
 });
 
-closeMobMenu.addEventListener('click', function () {
+closeMobMenu.addEventListener('click', function selectCloseBtn() {
   mobileMenu[0].style.display = 'none';
 });
 
 for (const i of mobLinks) {
-  i.addEventListener('click', function select () {
+  i.addEventListener('click', function selectMenuEl() {
     mobileMenu[0].style.display = 'none';
   });
 }
