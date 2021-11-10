5d01a3eca63fa5cb23cff881b8927123be9594a5
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-10 15:43:51 -0500

    Add skeleton validation to contact form.

--- script.js
+++ script.js
@@ -3,7 +3,33 @@ const mobileMenu = document.getElementsByClassName('mobMenu');
 const closeMobMenu = document.getElementById('closeMenu');
 const mobLinks = document.querySelectorAll('.mobMenu > ul > li > a');
 const bodyEl = document.querySelector('body');
-console.log(bodyEl);
+const formEl = document.getElementById('contactForm');
+const emailMsg = formEl.querySelector('small');
+const emailForm =  formEl.querySelector('input[type="email"]');
+
+console.log(formEl);
+console.log(emailForm.value);
+console.log(emailMsg);
+
+formEl.addEventListener('submit', (e) => {
+  
+  console.log(emailForm.value);
+  if (!isUpperCase(emailForm.value)) {
+    e.preventDefault();
+    emailMsg.style.display = 'block';
+    emailMsg.textContent = 'Email needs to be lowercase';
+    console.log(emailMsg);
+  } else {
+    emailMsg.style.display = 'none';
+    //emailForm.value = '';
+  }
+});
+
+function isUpperCase(str) {
+  str.split('').forEach((letter) =>{
+    return (/^[^a-z]*$/).test(letter);
+  });
+} 
 
 hamMenu[0].addEventListener('click', () => {
   mobileMenu[0].style.display = 'block';
