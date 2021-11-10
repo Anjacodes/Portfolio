189a6639b2d932de81943d0f0be1507cdfebaee4
Anjacodes <anja_schmidt7@posteo.de>
2021-11-10 16:34:53 -0600

    Add Linters to Local Ev

--- script.js
+++ script.js
@@ -5,25 +5,22 @@ const mobLinks = document.querySelectorAll('.mobMenu > ul > li > a');
 const bodyEl = document.querySelector('body');
 const formEl = document.getElementById('contactForm');
 const emailMsg = formEl.querySelector('small');
-const emailForm =  formEl.querySelector('input[type="email"]');
-
-formEl.addEventListener('submit', (e) => {
-  
-  console.log(hasUpperCase(emailForm.value));
-  if (hasUpperCase(emailForm.value)) {
-    e.preventDefault();
-    emailMsg.style.display = 'block';
-    emailMsg.textContent = 'Email needs to be lowercase';
-    console.log(emailMsg);
-  } else {
-    emailMsg.style.display = 'none';
-  }
-});
+const emailForm = formEl.querySelector('input[type="email"]');
 
 function hasUpperCase(str) {
   const regExp = /[A-Z]/;
   return regExp.test(str);
-} 
+}
+
+formEl.addEventListener('submit', (e) => {
+  if (hasUpperCase(emailForm.value)) {
+    e.preventDefault();
+    emailMsg.style.display = 'block';
+    emailMsg.textContent = 'Email needs to be lowercase';
+  } else {
+    emailMsg.style.display = 'none';
+  }
+});
 
 hamMenu[0].addEventListener('click', () => {
   mobileMenu[0].style.display = 'block';
