77595ffe25aac28d365b855661cc5c3e3363c3de
Anjacodes <anja_schmidt7@posteo.de>
2021-11-11 10:02:40 -0600

    Fix Linter Errors

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const mobileMenu = document.getElementsByClassName('mobMenu');
 const closeMobMenu = document.getElementById('closeMenu');
 const mobLinks = document.querySelectorAll('.mobMenu > ul > li > a');
 const bodyEl = document.querySelector('body');
-const portfolio = document.querySelectorAll('.card_background');
-const portfolio_nodes = document.querySelectorAll('.card_background').children;
-const heading = document.createElement('h3');
-const projects_section = document.getElementById('projects');
 
 hamMenu[0].addEventListener('click', () => {
   mobileMenu[0].style.display = 'block';
@@ -36,7 +32,7 @@ const content01 = {
   linkSource: 'https://github.com/Anjacodes/Portfolio',
   image: 'images/Img_placeholder.png',
   closeBtn: 'images/close_modal.png',
-  cover: 'images/snapshot_portfolio.png'
+  cover: 'images/snapshot_portfolio.png',
 };
 
 const content02 = {
@@ -50,7 +46,7 @@ const content02 = {
   linkSource: '',
   image: 'images/Img_placeholder.png',
   closeBtn: 'images/close_modal.png',
-  cover: 'images/snapshot_portfolio.png'
+  cover: 'images/snapshot_portfolio.png',
 };
 
 const content03 = {
@@ -64,7 +60,7 @@ const content03 = {
   linkSource: '',
   image: 'images/Img_placeholder.png',
   closeBtn: 'images/close_modal.png',
-  cover: 'images/snapshot_portfolio.png'
+  cover: 'images/snapshot_portfolio.png',
 };
 
 const content04 = {
@@ -78,7 +74,7 @@ const content04 = {
   linkSource: '',
   image: 'images/Img_placeholder.png',
   closeBtn: 'images/close_modal.png',
-  cover: 'images/snapshot_portfolio.png'
+  cover: 'images/snapshot_portfolio.png',
 };
 
 const content05 = {
@@ -92,7 +88,7 @@ const content05 = {
   linkSource: '',
   image: 'images/Img_placeholder.png',
   closeBtn: 'images/close_modal.png',
-  cover: 'images/snapshot_portfolio.png'
+  cover: 'images/snapshot_portfolio.png',
 };
 
 const content06 = {
@@ -106,98 +102,85 @@ const content06 = {
   linkSource: '',
   image: 'images/Img_placeholder.png',
   closeBtn: 'images/close_modal.png',
-  cover: 'images/snapshot_portfolio.png'
+  cover: 'images/snapshot_portfolio.png',
 };
 
 const projects = [content01, content02, content03, content04, content05, content06];
 
-function html_worksTemplate(index) {
-  return ' \
-  <div class="card_background"> \
-    <img class="card_image" src='+index.image+'> \
-    <h3 class="card_heading">'+index.name+'</h3> \
-    <h4 class="card_subheading">'+index.subheading+'</h4> \
-    <ul class="lang_container"> \
-      <li class="language_btn">'+index.technologies[0]+'</li> \
-      <li class="language_btn">'+index.technologies[1]+'</li> \
-      <li class="language_btn">'+index.technologies[2]+'</li> \
-      <li class="language_btn">'+index.technologies[3]+'</li> \
-    </ul> \
-    <a class="button button_modal">See Project</a> \
-  </div>';
+function htmlWorksTemplate(index) {
+  return `
+  <div class="card_background">
+    <img class="card_image" src='${index.image}'>
+    <h3 class="card_heading">${index.name}</h3>
+    <h4 class="card_subheading">${index.subheading}</h4>
+    <ul class="lang_container">
+      <li class="language_btn">${index.technologies[0]}</li>
+      <li class="language_btn">${index.technologies[1]}</li>
+      <li class="language_btn">${index.technologies[2]}</li>
+      <li class="language_btn">${index.technologies[3]}</li>
+    </ul>
+    <a class="button button_modal">See Project</a>
+  </div>`;
 }
 
-function modal_window_template (index) {
-  return ' \
+function modalWindowTemplate(index) {
+  return `
   <div class="modal_window_content"> \
-    <img id="close" class="close_btn" src='+index.closeBtn+'> \
-    <img class="cover_foto" src='+index.cover+'>\
+    <img id="close" class="close_btn" src='${index.closeBtn}'> \
+    <img class="cover_foto" src='${index.cover}'>\
     <div class="heading_cont"> \
-      <h3 class="modal_heading">'+index.title+'</h3> \
-      <a class="project_btn" href='+index.linkLive+' target="_blank">See Live<span class="btn_span"><img src="images/live_icon.png"></span></a> \
-      <a class="project_btn" href='+index.linkSource+' target="_blank">See Source<span class="btn_span"><img src="images/github_icon.png"></span></a> \
+      <h3 class="modal_heading">${index.title}</h3> \
+      <a class="project_btn" href=${index.linkLive} target="_blank">See Live<span class="btn_span"><img src="images/live_icon.png"></span></a> \
+      <a class="project_btn" href=${index.linkSource} target="_blank">See Source<span class="btn_span"><img src="images/github_icon.png"></span></a> \
     </div> \
     <ul class="modal_lang_container"> \
-      <li class="modal_language_btn">'+index.project_tech[0]+'</li> \
-      <li class="modal_language_btn">'+index.project_tech[1]+'</li> \
-      <li class="modal_language_btn">'+index.project_tech[2]+'</li> \
-      <li class="modal_language_btn">'+index.project_tech[3]+'</li> \
-      <li class="modal_language_btn">'+index.project_tech[4]+'</li> \
-      <li class="modal_language_btn">'+index.project_tech[5]+'</li> \
+      <li class="modal_language_btn">${index.project_tech[0]}</li> \
+      <li class="modal_language_btn">${index.project_tech[1]}</li> \
+      <li class="modal_language_btn">${index.project_tech[2]}</li> \
+      <li class="modal_language_btn">${index.project_tech[3]}</li> \
+      <li class="modal_language_btn">${index.project_tech[4]}</li> \
+      <li class="modal_language_btn">${index.project_tech[5]}</li> \
     </ul> \
-    <p class="project_description">'+index.description+'</p> \
+    <p class="project_description">${index.description}</p> \
     <div class="deskBtn-container"> \
-      <a class="project_btn" href='+index.linkLive+' target="_blank">See Live<span class="btn_span"><img src="images/live_icon.png"></span></a> \
-      <a class="project_btn" href='+index.linkSource+' target="_blank">See Source<span class="btn_span"><img src="images/github_icon.png"></span></a> \
+      <a class="project_btn" href=${index.linkLive} target="_blank">See Live<span class="btn_span"><img src="images/live_icon.png"></span></a> \
+      <a class="project_btn" href=${index.linkSource} target="_blank">See Source<span class="btn_span"><img src="images/github_icon.png"></span></a> \
     </div> \
-  </div>';
+  </div>`;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
   projects.forEach((element) => {
-    let dinContent = document.createDocumentFragment();
-    var newDiv = document.createElement('div');
+    const dinContent = document.createDocumentFragment();
+    const newDiv = document.createElement('div');
     newDiv.className = 'works_card';
 
-    newDiv.innerHTML = html_worksTemplate(element);
+    newDiv.innerHTML = htmlWorksTemplate(element);
     dinContent.appendChild(newDiv);
     document.getElementById('projects').appendChild(dinContent);
   });
-  const open_modal = document.querySelectorAll('.button');
+  const openModal = document.querySelectorAll('.button_modal');
 
-  open_modal.forEach((btn, i) => {
+  openModal.forEach((btn, i) => {
     btn.addEventListener('click', () => {
-      console.log(projects[i], i);
       bodyEl.style.overflow = 'hidden';
-      let modalContent = document.createDocumentFragment();
-      let newCont = document.createElement('div');
+      const modalContent = document.createDocumentFragment();
+      const newCont = document.createElement('div');
       newCont.className = 'modal_window_container';
-      newCont.innerHTML = modal_window_template(projects[i]);
+      newCont.innerHTML = modalWindowTemplate(projects[i]);
       modalContent.appendChild(newCont);
       document.getElementById('projects').appendChild(modalContent);
       document.getElementById('modal_background').classList.add('modal_active');
 
       // Add Event Listener To Close Modal Button
-      document.getElementById("close").addEventListener('click', () => {
-        document.getElementById('modal_background').classList.remove('modal_active');
-        newCont.style.display = 'none';
-        location.reload();
-      })
-    })
+      const closeBtn = document.querySelectorAll('.close_btn');
+      closeBtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+          document.getElementById('modal_background').classList.remove('modal_active');
+          newCont.style.display = 'none';
+          bodyEl.style.overflow = 'auto';
+        });
+      });
+    });
   });
 });
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   projects.forEach((element) => {
-//     let dinContent = document.createDocumentFragment();
-//     var newDiv = document.createElement('div');
-//     newDiv.className = 'works_card';
-
-//     newDiv.innerHTML = html_worksTemplate(element);;
-//     dinContent.appendChild(newDiv);
-//     document.getElementById('projects').appendChild(dinContent);
-//   });
-// });
-
